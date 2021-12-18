1ba8f7086eae77aebb2621171de0188aa0c53b00
khaoula <khaoulajabour4@gmail.com>
2021-12-18 02:45:07 -0500

    add github property

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,40 +1,41 @@
 const Engineer = require('../lib/Engineer');
 
 test('create employee object', () => {
-    const engineer =new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+    const engineer =new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
     expect(engineer.name).toBe('Khaoula');
     expect(engineer.id).toBe(11);
     expect(engineer.email).toBe('khaoula@jabour.com');
+    expect(engineer.github).toBe('khaoulaja');
 
 });
 
 test("gets engineer's name", () => {
-    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
     expect(engineer.getName()).toEqual(expect.stringContaining(engineer.name));
 });
 
 test("gets engineer's id", () => {
-    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
     expect(engineer.getId()).toEqual(expect.stringContaining(engineer.id.toString()));
 });
 
 test("gets engineer's email", () => {
-    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
     expect(engineer.getEmail()).toEqual(expect.stringContaining(engineer.email));
 });
 
-test("gets engineer's office number", () => {
-    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+test("gets engineer's GitHub", () => {
+    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
-    expect(engineer.getOfficeNumber()).toEqual(expect.stringContaining(engineer.officeNum.toString()));
+    expect(engineer.getGitHub()).toEqual(expect.stringContaining(engineer.github));
 });
 
 test("gets engineer's role", () => {
-    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com',12);
+    const engineer = new Engineer('Khaoula', 11, 'khaoula@jabour.com','khaoulaja');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
